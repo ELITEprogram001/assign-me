4a2365fb26b80407d932bbbc73d68d78c2c9d58f
ELITEprogram001 <ridgeway.jared@gmail.com>
2021-10-13 11:09:47 -0500

    first push

---
+++ AssignMe/AssignMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignMeApp.swift
+//  AssignMe
+//
+//  Created by Ridgeway, Dalton D on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AssignMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
