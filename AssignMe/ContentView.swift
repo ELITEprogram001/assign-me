06616560714acd9989993da429e0fde2350dd11d
Tai Tran <taivinh8@gmail.com>
2021-10-13 11:54:24 -0500

    Tai change to line 18, 19

--- AssignMe/ContentView.swift
+++ AssignMe/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
             .padding()
         Text("This is a betterer test.")
             .padding()
+        Text("This is BESTEST test. (tai)")
+        .padding()
     }
 
 }
