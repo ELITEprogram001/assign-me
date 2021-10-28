8ed707c37ed532e893a56f60d57566f590a84d00
Tommie Arceneaux <tommiearceneaux@yahoo.com>
2021-10-27 22:27:19 -0500

    No real changes

--- AssignMe/CategoryCard.swift
+++ AssignMe/CategoryCard.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import Foundation
 
+//comment
+
 var Testing = Category(name: "Fitness", color: "red") //comment out when no longer needed for designing category card
 //var Cat_Task_Count = Int count of items in list of tasks assigned to specific category
 //var Cat_Task_Count = 13 //placeholder test for Cat_Task_Count
