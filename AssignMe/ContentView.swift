a74e3b93234b0b9393513c9c3f3cba5bb5942215
tai <taivinh8@gmail.com>
2021-10-28 00:00:20 -0500

    finish implemented the TabView Bar at the bottom.( included space holder for other view file)

--- AssignMe/ContentView.swift
+++ AssignMe/ContentView.swift
@@ -12,38 +12,87 @@ struct ContentView: View {
     var body: some View {
 
         TabView{
-            //Weekly()
+            //CategoryCreationFormView()
+            WeeklyView()
                 .tabItem {
-                    Text("default")
-                    Image("home-alt-plus")
+                    Text("Weekly View")
+                    Image(systemName: "homekit")
                 }
-            //Calendar()
+            CalendarView()
                 .tabItem {
                     Text("calendar")
                     Image(systemName: "calendar")
                 }
-            //TaskEntry()
+            TaskEntry()
                 .tabItem {
                     Text("Task Entry")
-                    Image(systemName: "plus.app.fill").imageScale(.large)
+                    Image(systemName: "plus.circle.fill")
                 }
-            //Category()
+            CategoryView()
                 .tabItem {
                     Text("Category List")
-                    Image(systemName: "")
+                    Image(systemName: "square.grid.2x2.fill")
                 }
-            //Profile()
+            Profile()
                 .tabItem {
                     Text("Profile")
                     Image(systemName: "person.fill")
                 }
 
-        }//end body
-    }
-}
+        }   //end tabview
+    }//end body
+    
+}//end Contenview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
+///#################### embedded view within a view JUST A PLACE HOLDER
+struct WeeklyView: View {
+    var body: some View {
+        Text("Weekly VIEW")
+    }
+}
+
+struct CalendarView: View {
+    var body: some View {
+        Text(" Calendar VIEW")
+    }
+}
+
+struct TaskEntry: View {
+    var body: some View {
+        Text("TASK ENTRY")
+    }
+}
+
+struct CategoryView: View {
+    var body: some View {
+        NavigationView{
+            Text("category view")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        
+                        NavigationLink("add", destination: CategoryCreationFormView())
+                        
+                    }
+                }
+            
+                
+        }
+    }
+}
+struct Profile: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+
+
+
