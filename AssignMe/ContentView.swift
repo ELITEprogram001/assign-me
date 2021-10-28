384898603c68b175ef499fbe112ec819b7375d64
Sparky76015 <rohanmanimala@yahoo.com>
2021-10-27 21:24:51 -0500

    Update ContentView.swift

--- AssignMe/ContentView.swift
+++ AssignMe/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 struct ContentView: View {
+<<<<<<< HEAD
     // Test Category
     var Mental_Health = Category(name:"Mental Health",color:"Blue")
     var testing=true
@@ -27,10 +28,38 @@ struct ContentView: View {
                         }
             }
             
+=======
+    
+    var body: some View {
+        TabView{
+            Weekly()
+                .tabItem {
+                    Text("default")
+                    Image("home-alt-plus")
+                }
+            Calendar()
+                .tabItem {
+                    Text("calendar")
+                    Image(systemName: "calendar")
+                }
+            TaskEntry()
+                .tabItem {
+                    Text("Task Entry")
+                    Image(systemName: "plus.app.fill").imageScale(.large)
+                }
+            Category()
+                .tabItem {
+                    Text("Category List")
+                    Image(systemName: "")
+                }
+            Profile()
+                .tabItem {
+                    Text("Profile")
+                    Image(systemName: "person.fill")
+                }
+>>>>>>> 7acf0f75e7d7fc1728117518f1eb576714d90776
         }
     }
-    
-
 }
 
 struct ContentView_Previews: PreviewProvider {
