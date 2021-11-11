39e30987585126f4a45042d4b03a6173c8569b78
Tommie Arceneaux <tommiearceneaux@yahoo.com>
2021-11-10 23:26:02 -0600

    TEST PUSH

--- AssignMe/Tommie/CategoryCard.swift
+++ AssignMe/Tommie/CategoryCard.swift
@@ -12,7 +12,7 @@ import Foundation
 
 //var Testing = Category(name: "Fitness", color: .red) //comment out when no longer needed for designing category card
 //var Cat_Task_Count = Int count of items in list of tasks assigned to specific category
-//var Cat_Task_Count = 13 //placeholder test for Cat_Task_CounttT
+//var Cat_Task_Count = 13 //placeholder test for Cat_Task_CounttTT
 
 
 
