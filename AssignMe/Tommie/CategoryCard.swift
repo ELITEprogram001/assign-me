a3ef1a6fbc4ac3f2721f895ca42920bd9b1351fb
Tommie Arceneaux <tommiearceneaux@yahoo.com>
2021-11-28 01:41:32 -0600

    Category Card changes to font sizes and positioning of text for category name and 'tasks: #'

--- AssignMe/Tommie/CategoryCard.swift
+++ AssignMe/Tommie/CategoryCard.swift
@@ -39,14 +39,13 @@ struct CategoryCard: View {
     var catIndex: Int
 
     func del () {
-        for index in 0..<user.taskList.count {
+        for index in 1..<user.taskList.count {
             if (user.taskList[index].category.id == user.categoryList[catIndex].id){
                 user.taskList[index].category = user.categoryList[0]
             }
         }
         
         user.categoryList.remove(at: catIndex)
-        //self.presentation.wrappedValue.dismiss()
     }
     
     func indexCheck () ->Category {
@@ -66,12 +65,12 @@ struct CategoryCard: View {
     func taskNum() -> Int {
         var count = 0
         for index in 1..<user.taskList.count {
-            if (user.taskList[index].category.name == user.categoryList[catIndex].name){
+            if (user.taskList[index].category.name == user.categoryList[intCheck()].name){
                 count = count + 1
             }
         }
         return count
-    }
+    } //taskNum END
     
     var body: some View {
         ZStack{ //Stacks all text, buttons, and other details; starting from bottom to top
@@ -81,27 +80,47 @@ struct CategoryCard: View {
                 .shadow(radius: 6) //optional
             
             
+            
+            /*      Text("\(category.name)") //variable category name
+             .font(.system(size: 30, weight: .light, design: .serif))
+             
+             Text("\(category.name)") //variable category name
+             .font(.custom("Montserrat-Regular", size: 30))
+             //.font(.system(size: 30, weight: .light, design: ))
+             
+             .fontWeight(.bold)
+             .foregroundColor(Color.white)
+             .multilineTextAlignment(.leading)
+             .lineLimit(2)
+             //.frame(minWidth: 0, idealWidth: 150, maxWidth: 150, minHeight: 0, idealHeight: 3, maxHeight: 5, alignment: .leading)
+             .frame(width: 150, height: 50, alignment: .leading)
+             .minimumScaleFactor(0.39)
+             .padding(.leading, -125.0)
+             .padding (.bottom, 2)  */
+            
+            
             Text("\(category.name)") //variable category name
-                .font(.custom("Montserrat-Regular", size: 30))
-                //.font(.system(size: 30, weight: .light, design: ))
+                .font(.system(size: 21, weight: .light, design: .serif))
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
-                //.frame(minWidth: 0, idealWidth: 150, maxWidth: 150, minHeight: 0, idealHeight: 3, maxHeight: 5, alignment: .leading)
-                .frame(width: 150, height: 50, alignment: .leading)
+                // .frame(minWidth: 0, idealWidth: 150, maxWidth: 150, minHeight: 1, idealHeight: 1, maxHeight: 1, alignment: .leading)
+                .frame (width: 150, height: 50, alignment: .leading)
                 .minimumScaleFactor(0.39)
                 .padding(.leading, -125.0)
-                .padding (.bottom, 2)
+                .padding (.bottom, 21)
+            
+            
             
             
             Text("\nTasks: \(taskNum())") //"\nTasks: (2)" number is a placeholder for now
-                       .font(.system(size: 21, weight: .light, design: .serif))
-                       .fontWeight(.bold)
-                       .foregroundColor(Color.white)
-                       .multilineTextAlignment(.leading)
-                       .padding(.leading, -139.0)
-                       .padding(.bottom, -30)
+                .font(.system(size: 16, weight: .light, design: .serif))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .multilineTextAlignment(.leading)
+                .padding(.leading, -139.0)
+                .padding(.top, 50.0)
             
             
 //-------------------------------KEEP ME FOR NOW 4:03 AM 23 November 2021 ---------------------
