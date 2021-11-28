5a4ff69f68c9c0c6e20204b9d50a96bd492fe2a0
ELITEprogram001 <ridgeway.jared@gmail.com>
2021-11-27 21:52:49 -0600

    orange

--- AssignMe/Dalton/CustomColors.swift
+++ AssignMe/Dalton/CustomColors.swift
@@ -12,7 +12,7 @@ extension Color {
     static let bg_light = Color.init(red: 45/255, green: 45/255, blue: 45/255)
     static let blew = Color.init(red: 131/255, green: 201/255, blue: 244/255)
     static let near_black = Color.init(red: 10/255, green: 10/255, blue: 10/255)
-    static let bright_maroon = Color.init(red: 185/255, green: 49/255, blue: 79/255)
+    static let bright_maroon = Color.init(red: 235/255, green: 69/255, blue: 17/255)
     
     static var primary_color = Color.init(red: 0, green: 0, blue: 0)
 }
--- AssignMe/Tommie/TaskCard.swift
+++ AssignMe/Tommie/TaskCard.swift
@@ -1,4 +1,3 @@
-//
 //  TaskCard.swift
 //  AssignMe
 //
